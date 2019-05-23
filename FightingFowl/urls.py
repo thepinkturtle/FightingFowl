cbbfd13abcc0f9bccf6031d41e1bd77f00f35a9f
thepinkturtle <devinlax@hotmail.com>
2019-05-23 09:33:04 -0700

    remove index and update paths

--- FightingFowl/urls.py
+++ FightingFowl/urls.py
@@ -1,6 +1,7 @@
 """
 Definition of urls for FightingFowl.
 """
+
 from django.conf.urls import include, url
 from StockerChecker import views
 from django.contrib import admin
@@ -8,8 +9,7 @@ admin.autodiscover()
 
 urlpatterns = [
 
-    url(r'^$', views.index, name='index'),
-    url(r'^form/$', views.model_form_upload, name='model_form_upload'),
+    url(r'^$', views.model_form_upload, name='model_form_upload'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^form/parse_json$', views.parse_json, name='parse_json'),
+    url(r'^parse_json', views.parse_json, name='parse_json')
 ]
